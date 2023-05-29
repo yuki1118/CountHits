83a36a44809e75dec78c2baf648a5ce19859e8ca
yuki1118 <77534102+yuki1118@users.noreply.github.com>
2023-05-29 22:33:23 +0900

    Initial Commit

---
+++ countHits/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  countHits
+//
+//  Created by 吉田侑己 on 2022/11/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+                .font(.largeTitle)
+                .foregroundColor(Color.red)
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
